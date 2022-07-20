8a7c4c62656c243f3058be242965607cfcba554d
serdartas <serdartas@hotmail.com>
2022-07-20 14:51:59 +0200

    code

---
+++ src/main/java/com/tollstation/configservergit/ConfigserverGitApplication.java
@@ -0,0 +1,14 @@
+package com.tollstation.configservergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ConfigserverGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverGitApplication.class, args);
+	}
+
+}
