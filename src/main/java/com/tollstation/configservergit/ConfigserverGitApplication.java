5e9d1e84679022302be1c1d71fdf32821d4600ff
serdartas <serdartas@hotmail.com>
2022-07-20 21:56:40 +0200

    enable config server added

--- src/main/java/com/tollstation/configservergit/ConfigserverGitApplication.java
+++ src/main/java/com/tollstation/configservergit/ConfigserverGitApplication.java
@@ -2,8 +2,9 @@ package com.tollstation.configservergit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigserverGitApplication {
 
